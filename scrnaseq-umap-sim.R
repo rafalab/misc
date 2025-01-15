fc2a7bc9a6d2439b4e03d16ea3ec61e7d580f345
Rafael A Irizarry <rafa@ds.dfci.harvard.edu>
2025-01-15 13:17:37 -0500

    adding single cell RNASeq UMAP simulation

--- scrnaseq-umap-sim.R
+++ scrnaseq-umap-sim.R
@@ -1,83 +1,75 @@
 library(MASS)
 library(Matrix)
+library(matrixStats)
 library(ggplot2)
-plots <- TRUE
 
-set.seed(2024-1-12)  # For reproducibility
-p <- 5000 ## number of genes
+## Get a sense of data to creata realisti simulation
+plots <- TRUE
+  
+set.seed(2024 - 1 - 12)  # For reproducibility
+p <- 15000 ## number of genes
 
 ## To mimic gene networks we will create a block diagonal covariance matrix
-ps <- round(p*c(0.05, 0.04, 0.03, 0.02, 0.01, rep(0.005, 25)))
-
+ps <- round(p*c(0.005, 0.004, 0.003, 0.002, 0.001, rep(0.0005, 25)))
+p1 <- sum(ps)
+p0 <- round((p - p1)*.75)
+p00 <- p - p0 - p1
 mats <- lapply(ps, function(pp){
-  P <- matrix(runif(1, 0.25, .66), pp, pp)
-  diag(P) <- 1
-  s <- pmin(1.5, sqrt(1/rgamma(pp, 2, 1))) # some genes vary more than others
-  m <- P*outer(s,s)
-  return(m)
+  corr <- matrix(runif(1, 0.25, .66), pp, pp) ## correlation
+  diag(corr) <- 1
+  s <- sqrt(1/rgamma(pp, 4, 4)) # some genes vary more than others
+  return(corr*outer(s,s))
 })
-
-## n0 inactive genes
-n1 <- sum(ps)
-n0 <- p - n1
-
 # Combine blocks into a block diagonal covariance matrix
-library(Matrix)
 cov_matrix <- bdiag(mats)
 
+options(future.globals.maxSize = 2 * 1024^3)
 n_samples <- 10000  # Number of samples
 ## generate correlated data
 ## each gene has same expected value across all cells:
 ##this implies there are NO CLUSTERS
-##note: we have n1 on genes and n0 off genes, that are independent
-odata <- cbind(mvrnorm(n_samples, mu = pmin(1,rnorm(n1, -2, 1)), Sigma = cov_matrix),
-               sweep(
-                 sweep(
-                   matrix(rnorm(n_samples*n0), n_samples, n0), 2, 
-                   1/rgamma(n0, 5, .2), FUN = "*"), 2, 
-                 rnorm(n0, -4, 0.5), FUN = "+"))
+##note: we have p1 "on" genes that are correlated with other genes
+## and p0 off genes, and p00 completely off genes that are independent and have no variance. Sampling variation added later.
+## the genes on the rows for Seurat
+odata <- rbind(
+  t(mvrnorm(n_samples, mu = runif(p1, -8, -4), Sigma = cov_matrix)),
+    matrix(rnorm(n_samples*p0), p0, n_samples)*sqrt(1/rgamma(p0, 3, .7)) + rnorm(p0, -12, 1.5),
+    matrix(0, p00, n_samples) + rnorm(p00, -15, 1))
              
 ## Total coverage is different from cell to cell
-## Generate data from mixture model to mimic experimtail data
-K <- 10
-weights <- runif(K, 0.25, 0.75); weight <- weights/sum(weights)
-means <- rnorm(K, 2.75, 0.75)
-sds <- rep(0.1, K)
-z <- sample(1:K, size = n_samples/K, replace = TRUE, prob = weights)
-coverage <-  rnorm(n_samples, mean = means[z], sd = sds[z])
+## Generate data from mixture model to mimic experimental data
+K <- 5
+weights <- runif(K, 0.25, 0.75); weights <- weights/sum(weights)
+means <- log(10^seq(3,5, len = K))
+s <- rep(0.3, K)
+z <- sample(1:K, size = n_samples, replace = TRUE, prob = weights)
+coverage <-  rnorm(n_samples, mean = means[z], sd = s[z])
 ## add log offset to mimic different coverate
-data <- odata + coverage
-## genes on the rows for Seurat
-data <- t(data)
+data <- sweep(odata, 2, coverage, FUN = "+")
+data[data>log(200)] <- log(200) ##remove outliers
 ## Generate counts using Poisson variation
 counts <- matrix(rpois(length(unlist(data)), exp(unlist(data))), 
                  nrow(data), ncol(data))
-if(plots){ ##checks
-  min(colSums(counts))
-  hist(log10(colSums(counts)),nc=200)
-  hist(colMeans(counts==0))
-  hist(coverage,nc=200)
-  hist(log10(rowMeans(counts)), nc = 100)
-  hist(apply(log10(counts+1), 1, sd), nc = 100)
-}
 ## Leverage sparsity and add rownames and colnames
 counts <- as(counts, "dgCMatrix")
 gc();gc() ## Garbage collection after making smaller object
 rownames(counts) <- paste0("Gene", 1:nrow(counts))
 colnames(counts) <- paste0("Cell", 1:ncol(counts))
 
-## Follow standard Suerat pipeline with defaults
+## Simulation ends here
+
+## Follow standard Suerat pipeline 
 library(Seurat)
 seurat_obj <- CreateSeuratObject(counts = counts)
 seurat_obj <- SCTransform(seurat_obj)
 seurat_obj <- RunPCA(seurat_obj, features = VariableFeatures(object = seurat_obj))
 if(plots) ElbowPlot(seurat_obj)
-K <- 30
-seurat_obj <- FindNeighbors(seurat_obj, dims = 1:K)
+D <- 30
+seurat_obj <- FindNeighbors(seurat_obj, dims = 1:D)
 seurat_obj <- FindClusters(seurat_obj)  
 
 ## change UMAP parameters so local structure is prioritized
-seurat_obj <- RunUMAP(seurat_obj, dims = 1:K, n.neighbors = 15, min.dist = 0.1)
+seurat_obj <- RunUMAP(seurat_obj, dims = 1:D, n.neighbors = 15, min.dist = 0.1)
 p1 <- DimPlot(seurat_obj, reduction = "umap", group.by = "seurat_clusters") +
   ggtitle("n.neighbors = 15, min.dist = 0.1")
 print(p1)
@@ -85,7 +77,7 @@ print(p1)
 ggsave(p1, filename = "~/Desktop/umap.png", width  = 10, height = 7.5)
 
 ## run with defaults
-seurat_obj <- RunUMAP(seurat_obj, dims = 1:K)
+seurat_obj <- RunUMAP(seurat_obj, dims = 1:D)
 p0 <- DimPlot(seurat_obj, reduction = "umap", group.by = "seurat_clusters") + 
   ggtitle("n.neighbors = 30, min.dist = 0.3")
 print(p0) 
@@ -94,5 +86,52 @@ ggsave(p0, filename = "~/Desktop/umap-0.png", width  = 10, height = 7.5)
 
 
 
+## CHECKS
+if(plots){
+  ### check that simnulations mimics a real dataset
+  data("pbmc_facs", package = "fastTopics")
+  cov <- rowSums(pbmc_facs$counts)
+  x <- pbmc_facs$counts/cov
+  mu <- colMeans(x)
+  sds <- colSds(as.matrix(log(x+min(x[x>0])/2)))
+  
+  ## Is SD for expressed genes similar to simulation
+  rafalib::mypar(1,2)
+  hist(sds[mu > -8], nc = 25, xlim = c(0,3))
+  hist(sqrt(diag(cov_matrix)), nc = 25, xlim = c(0,3)) 
+  
+  ## Is distribution of mean expression for genes similar 
+  rafalib::mypar(2,1)
+  hist(log(mu),nc = 100,xlim = c(-18,-3))
+  hist(rowMeans(odata),nc = 100,xlim = c(-18,-3))
+  
+  ## is coverage in the same range. We want it a bit higher
+  rafalib::mypar(2,1); 
+  hist(log(cov),xlim=range(log(cov)),nc=100)
+  hist(coverage,nc=100)
+  
+  ## miniumu coverage not too small
+  print(min(colSums(counts)))
+  
+  rafalib::mypar(1,1)
+  ## % 0s by cell
+  hist(colMeans(counts==0), nc =100)
+  ## log mean counts match real dataset
+  rafalib::mypar(2,1)
+  hist(log(rowMeans(counts)), nc = 100)
+  hist(log(mu), nc = 100)
+  rafalib::mypar(1,1)
+  qqplot(log(mu), log(rowMeans(sweep(counts, 2, colSums(counts), FUN = "/"))));abline(0,1)
+  ## sd of log counts match real dataset
+  rafalib::mypar(2,1)
+  hist(apply(log(counts+1), 1, sd), nc = 100, xlim = c(0,4))
+  hist(sds, nc = 100, xlim = c(0,4))
+  rafalib::mypar(1,1)
+  qqplot(sds, apply(log(counts+1), 1, sd));abline(0,1)
+  ## observed coverage seem realistic?
+  ## should be between 1.000 and 100,000
+  hist(log10(colSums(counts)),nc=100)
+}
+  
 
 
